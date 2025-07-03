991bfb746dbd1cd76f056d00783668e979db3ce4
halim-afk <a.alouah0243@uca.ac.ma>
2025-07-03 12:37:11 +0100

    add task_3.sql

--- task_3.sql
+++ task_3.sql
@@ -1,3 +1,4 @@
-SELECT TABLE_NAME
-FROM INFORMATION_SCHEMA.TABLES
-WHERE TABLE_SCHEMA = 'alx_book_store';
+-- task_3.sql
+-- This script lists all tables in the alx_book_store database.
+
+SHOW TABLES;
